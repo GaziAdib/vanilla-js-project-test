8d9b17ff3a621795ea2214715996509df5013c4d
Inflowchain <92274055+Inflowchain@users.noreply.github.com>
2022-08-13 22:17:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,167 +1,144 @@
-console.log('done');
+console.log("done");
 
-const rootDiv = document.querySelectorAll('.root-div');
+const rootDiv = document.querySelectorAll(".root-div");
 
 console.log(rootDiv);
 
-
-const countEl = document.querySelector('.countstate');
-const incBtn = document.querySelector('.increment-btn');
-const decBtn = document.querySelector('.decrement-btn');
-const addNewCountBtn = document.querySelector('.add-counter');
-
+const countEl = document.querySelector(".countstate");
+const incBtn = document.querySelector(".increment-btn");
+const decBtn = document.querySelector(".decrement-btn");
+let addNewCountBtn = document.querySelectorAll(".add-counter");
 
 // action indentifier
 
-const INCREMENT = 'increment';
-const DECREMENT = 'decrement';
-const ADD_COUNT = 'ADD_COUNT';
-
-
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+const ADD_COUNT = "ADD_COUNT";
 
 // action creator
 
 const increment = (value) => {
-    return {
-        type: INCREMENT,
-        payload: value
-    };
-}
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
 
 const decrement = (value) => {
-    return {
-        type: DECREMENT,
-        payload: value
-    };
-}
-
+  return {
+    type: DECREMENT,
+    payload: value,
+  };
+};
 
 const addCount = (id, value) => {
-    return {
-        type: ADD_COUNT,
-        payload: {
-            id: id,
-            value: value
-        }
-    }
-}
+  return {
+    type: ADD_COUNT,
+    payload: {
+      id: id,
+      value: value,
+    },
+  };
+};
 
 const initialState = [
+  (counter = [
+    {
+      id: 1,
+      value: 0,
+    },
+  ]),
+];
 
-    counter = [
-        {
-            id: 1,
-            value: 0,
-        }
-    ]
-
-]
-    
-
-const countReducer = (state=initialState, action) => {
-
-    switch (action.type) {
-        case INCREMENT:
-            return {
-                ...state,
-                value: state.value + action.payload
-            }
-        case DECREMENT:
-            return {
-                ...state,
-                value: state.value - action.payload
-            }
-        case ADD_COUNT:
-            return {
-                ...state,
-                counter: state.push({
-                    id: action.payload.id,
-                    value: action.payload.value
-                })
-                
-            }      
-        default:
-            return state;
-    }
-
-}
-
-
+const countReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        value: state.value + action.payload,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        value: state.value - action.payload,
+      };
+    case ADD_COUNT:
+      return {
+        ...state,
+        counter: state.push({
+          id: action.payload.id,
+          value: action.payload.value,
+        }),
+      };
+    default:
+      return state;
+  }
+};
 
 // create redux store
-var store = Redux.createStore(countReducer)
-
+var store = Redux.createStore(countReducer);
 
 // create a function to render the ui
 
 const render = () => {
+  const state = store.getState();
 
-    const state = store.getState();
+  //countEl.innerText = state.value.toString();
+};
 
-    //countEl.innerText = state.value.toString();
-    
-}
+render();
 
-render()
-
-// subscribe to store 
-store.subscribe(render)
+// subscribe to store
+store.subscribe(render);
 
 // click events on the button
 
-incBtn.addEventListener('click', () => {
-    store.dispatch(increment(5))
+incBtn.addEventListener("click", () => {
+  store.dispatch(increment(5));
 });
 
-decBtn.addEventListener('click', () => {
-
-    store.dispatch(decrement(2))
-        
+decBtn.addEventListener("click", () => {
+  store.dispatch(decrement(2));
 });
-
 
 // create element
 
-var childDiv = document.createElement('div');
-
-
-childDiv.innerHTML +=`
-<div class="container text-center p-4 m-3 root-div2">
-            <h4 class="text-4xl text-center font-bold" id="countstate">0</h4>
-    
-            <div class="container text-center">
-                <span>
-                    <button class="p-2 m-2 border" id="increment-btn">increment</button>
-                    <button class="p-2 m-2 border" id="decrement-btn">decrement</button>
-                    <button class="p-2 m-2 border" id="add-counter">Add Counter+</button>
-                </span>
-            </div>
-        </div>`
-
 // add-counter button clicked
 
-addNewCountBtn.addEventListener('click', () => {
-    console.log('clicked');
+document.addEventListener("click", (e) => {
+  if (e.target && Array.from(e.target.classList).includes("add-counter")) {
+    console.log(addNewCountBtn.length, "el");
+    var childDiv = document.createElement("div");
+
+    childDiv.innerHTML += `
+ <div class="container text-center p-4 m-3 root-div2">
+        <h4 class="text-4xl text-center font-bold countstate" >
+          0
+        </h4>
+
+        <div class="container text-center">
+          <span>
+            <button class="p-2 m-2 border increment-btn" >
+              increment
+            </button>
+            <button class="p-2 m-2 border decrement-btn">
+              decrement
+            </button>
+            <button class="p-2 m-2 border add-counter" >
+              Add Counter+
+            </button>
+          </span>
+        </div>
+      </div>`;
+    console.log("clicked");
+    console.log(rootDiv);
 
     // for(var i = 0; i < rootDiv.length; i++) {
     //     rootDiv[i].appendChild(childDiv);
     // }
 
-
-    document.body.appendChild(childDiv)
-        
+    rootDiv[0].appendChild(childDiv);
+    addNewCountBtn = document.querySelectorAll(".add-counter");
+    console.log(addNewCountBtn);
+  }
 });
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var div = document.createElement('div');
-//     div.id = 'container';
-//     div.innerHTML = 'Hi there!';
-//     div.className = 'border pad';
- 
-//     document.body.appendChild(div);
-// }, false);
